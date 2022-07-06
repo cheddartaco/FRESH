328b15d2adef7bf60fd6bb55b5f45f0de08f1e47
Your Name <you@example.com>
2022-07-05 21:00:36 -0300

    ah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,30 @@
     <title>BD Alumnos</title>
 </head>
 <body>
-<h1>
+<center><h1> Formulario De Ingresantes </h1></center>
+
+<label for="">Nombre</label>
+
+<input class="form-control" type="text" name="nombre" id="" ><br>
+
+<label for=""> Apellido</label>
+<input class="form-control" type="text" name="apellido" id="" ><br>
+
+<label for=""> Dni</label>
+<input class="form-control" type="number" name="dni" id="" ><br>
+
+<label for=""> Cuil</label>
+<input class="form-control" type="number" name="cuil" id="" ><br>
+
+<label for=""> Fecha de nacimiento</label>
+<input class="form-control" type="date" name="fecha_nacimiento" id="" ><br>
+
+<label for="">Foto</label><br>
+
+<input class="form-control" type="file" name="foto" id="">
+<br>
+
+<input class="form-control" type="submit" value="Guardar">
+
 </body>
 </html>
